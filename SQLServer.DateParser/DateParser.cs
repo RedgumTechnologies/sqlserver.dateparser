0166f9b421ea3dd77c47f73891af1bf16bb9f000
Matt White <matt@redgum.com.au>
2018-03-02 18:50:10 +1100

    Initial commit of the core source code while sitting in the Virgin Lounge in Adelaide airport

---
+++ SQLServer.DateParser/DateParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Data;
+using Microsoft.SqlServer.Server;
+using System.Data.SqlTypes;
+
+class DateParser
+{
+    [Microsoft.SqlServer.Server.SqlProcedure]
+    public static SqlDateTime Parse(string text)
+    {
+        DateTime dt;
+        if (DateTime.TryParse(text, out dt))
+        {
+            return new SqlDateTime(dt);
+        }
+        else
+        {
+            return new SqlDateTime();
+        }
+    }
+
+    public static SqlBoolean TryParse(string text)
+    {
+        DateTime dt;
+        if (DateTime.TryParse(text, out dt))
+        {
+            return new SqlBoolean(true);
+        }
+        else
+        {
+            return new SqlBoolean(true);
+        }
+    }
+}
